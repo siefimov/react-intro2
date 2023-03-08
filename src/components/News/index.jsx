e05fcaf3d0a94b50ed49bf8e6e6061e20de177cb
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-03-08 18:04:45 +0200

    added minor changed

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-import './index.scss'
+import "./index.scss";
 
-const News = ()=>{
-  return(
+const News = () => {
+  return (
     <div className="news">
-      <h2 className="news__title title-h2">Blog</h2>
+      <h2 className="news__title title-h2">News</h2>
       <div className="news__item"></div>
       <div className="news__item"></div>
       <div className="news__item"></div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default News;
